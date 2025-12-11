f868c8744ebb785653e48f52d1343b0059157d5e
Migq1203r <153543396+Migq1203r@users.noreply.github.com>
2025-12-11 15:55:38 +0000

    Refactor card selection for players and computer

--- blackjack.py
+++ blackjack.py
@@ -12,10 +12,10 @@ def start():
     global cards_people_perm
     global cards_computer_perm
     if first_time == True:
-        cards_people = random.choices(cards,k=2) # Make the cards choices
+        cards_people = [random.choice(cards), random.choice(cards)] # Make the cards choices
         for card in cards_people:
             cards.remove(card)
-        cards_computer = random.choices(cards,k=2) # Make the computer cards choices
+        cards_computer = [random.choice(cards), random.choice(cards)] # Make the computer cards choices
         for card in cards_computer:
             cards.remove(card)
         cards_people_perm.append(cards_people)
@@ -30,19 +30,24 @@ def start():
     print(f"Computer's first card: {latest_computer[0]}")
     more_card = input("Type 'y' to get another card, type 'n' to pass [Y/n]: ")
     if more_card.lower() == "y":
-        card_people = random.choices(cards,k=1) # Make the cards choices
-        for card in card_people:
-            cards.remove(card)
-        card_computer = random.choices(cards,k=1)
-        for card in card_computer:
-            cards.remove(card)
-        print(cards)
+        card = random.choice(cards)
+        cards_people_perm[-1].append(card)
+        cards.remove(card)
+        print(card)
+        card = random.choice(cards)
+        cards_computer_perm[-1].append(card)
+        cards.remove(card)
+        print(card)
         os.system("clear")
         start()
     elif more_card.lower() == "n":
         print(f"Your final hand: {cards_people_perm}, current score: {sum(latest_hand)}")
         print(f"Computer's final hand: {cards_computer_perm}, current score: {sum(latest_computer)}")
-        if points_people > points_computer:
+        if points_people > 21:
+            print("You Lose!")
+        elif points_computer > 21:
+            print("You Win!")
+        elif points_people > points_computer:
             print("You Win!")
         elif points_people == points_computer:
             print("Draw 🙃")
@@ -53,7 +58,11 @@ def start():
         else:
             print("You Lose!") 
         dowantplay = input("Do you want play blackjack? [Y/n]: ")
+        os.system("clear")
+        cards.clear()
+        cards.extend([2,3,4,5,6,7,8,9,10,10,10,10,11])
         if dowantplay.lower() == "y":
+            first_time = True
             start()
         
 
